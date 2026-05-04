4d613d465820c3a2ca2a2c414c228dc6dc850481
Ivar Troost <ivar.troost@alliander.com>
2026-05-04 23:41:31 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://nlccs.nl',
+  site: 'https://deSagaz.github.io',
+  base: '/nlccs-website',
 });
