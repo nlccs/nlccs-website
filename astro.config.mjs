a2cb3abee59deba8dcc79de7147e27828eb87124
Ivar Troost <ivar.troost@alliander.com>
2026-05-06 00:17:53 +0200

    Set custom test domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://deSagaz.github.io',
-  base: '/nlccs-website',
+  site: 'https://beta.nlccs.nl',
 });
